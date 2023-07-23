6c378e06d5f3330fbfc2d485ba4222b076ef3902
EngrArfin <arfin.cse.green.edu.bd.gmail.com>
2023-07-23 07:56:12 +0600

    car up to date

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+
 import AddCoffee from './components/AddCoffee.jsx';
 import UpdateCoffee from './components/UpdateCoffee.jsx';
 
@@ -13,17 +15,19 @@ const router = createBrowserRouter([
 {
   path: "/",
   element: <App></App>,
+  loader: () => fetch('http://localhost:5000/coffee')
 },
 {
   path: "addCoffee",
-  element: <AddCoffee></AddCoffee>
+  element: <AddCoffee></AddCoffee>,
+  
 },
 {
-  path: "updateCoffee",
-  element: <UpdateCoffee></UpdateCoffee>
+  path: 'updateCoffee/:id',
+  element: <UpdateCoffee></UpdateCoffee>,
+  loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`)
 }
 ])
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
