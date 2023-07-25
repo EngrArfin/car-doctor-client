473943bbc3527732beaf413f90dfbb108e5b733b
EngrArfin <arfin.cse.green.edu.bd.gmail.com>
2023-07-25 10:42:38 +0600

    complete header and home

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
-import {
-  createBrowserRouter,
+import {  
   RouterProvider,
 } from "react-router-dom";
+import router from './Routes/Routes.jsx';
 
-import AddCoffee from './components/AddCoffee.jsx';
-import UpdateCoffee from './components/UpdateCoffee.jsx';
-
-const router = createBrowserRouter([
-{
-  path: "/",
-  element: <App></App>,
-  loader: () => fetch('http://localhost:5000/coffee')
-},
-{
-  path: "addCoffee",
-  element: <AddCoffee></AddCoffee>,
-  
-},
-{
-  path: 'updateCoffee/:id',
-  element: <UpdateCoffee></UpdateCoffee>,
-  loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`)
-}
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <div className='mx-w-7xl mx-auto'>
+    <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
+  </div>
 )
---
+++ src/pages/Home/Home/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import logo from '../../../../../assets/logo.svg'
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="navbar bg-base-100 mb-6">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <li><a>Home</a></li>
+      <li><a>About</a></li>
+      <li><a>Service</a></li>
+      <li><a>Blog</a></li>
+      <li><a>Contract</a></li>
+      </ul>
+    </div>
+    <Link to='/' className="btn btn-ghost normal-case text-xl">
+      <img src={logo} alt="" />
+    </Link>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li><a>About</a></li>
+      <li><a>Service</a></li>
+      <li><a>Blog</a></li>
+      <li><a>Contract</a></li>
+    </ul>
+  </div>
+  {/* cart */}
+  
+  <div className="navbar-end"> 
+  <label tabIndex={0} className="btn btn-ghost btn-circle">
+        <div className="indicator">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+          {/* <span className="badge badge-sm indicator-item">8</span> */}
+        </div>
+      </label>
+
+  <button className="btn btn-ghost btn-circle justify-right ">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    </button>
+    <button className="btn btn-outline btn-warning">Appointment</button>
+    
+  </div>
+</div>
+  );
+};
+
+export default NavBar;
